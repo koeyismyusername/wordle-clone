8a39c7fe8109daeb873d461a1ec658672fa9d0f2
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-05-22 10:41:07 +0900

    js 이름 변경, 상수 분리
    - create-word.js 이름을 functions.js로 변경
    - index.html 파일에 이름 바뀐 js 연결
    - CHARS, ANSWER 상수 분리

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,9 @@
-// 알파벳모음
-const chars = "QWERTYUIOPASDFGHJKLZXCVBNM";
-
-// 무작위로 5글자 단어 생성
-const answer = createRandomWord();
-console.log(answer);
-
 // 무작위로 5글자 단어를 생성하는 함수
 function createRandomWord() {
   let result = "";
   for (let i = 0; i < 5; ++i) {
     const index = Math.floor(Math.random() * 26);
-    result += chars[index];
+    result += CHARS[index];
   }
   return result;
 }
